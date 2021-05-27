58f8b7b15946444ef5d9911be8f1df101c475d84
solangeguzman <solangeguzmanduchen@gmail.com>
2021-05-27 16:09:33 +0200

    "Password" JS

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,4 @@ var nome = prompt("scrivi il tuo nome!");
 var cognome = prompt("scrivi il tuo cognome!");
 var colore = prompt ("scrivi il tuo colore preferito!");
 
-document.getElementById('Password').innerHTML = nome + ' ' + cognome + colore + (21);
+document.getElementById("Password").innerHTML = nome + cognome + colore + (21);
